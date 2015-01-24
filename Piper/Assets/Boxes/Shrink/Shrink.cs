098de19210eb6a2089c10bdfa4c55c0c9ff69628
Husenap <Hussein_t_97@hotmail.com>
2015-01-24 12:24:59 +0100

    shrinking cube

--- Piper/Assets/Boxes/Shrink/Shrink.cs
+++ Piper/Assets/Boxes/Shrink/Shrink.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class Shrink : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	private IsLookedAt detector;
 	
+	void Start () {
+		detector = GetComponent<IsLookedAt>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+		if(detector.Spotted){
+			iTween.ScaleBy(gameObject, iTween.Hash("x", .01, "y", .01, "z", .01, "easeType", "easeOutExpo", "Time", 1));
+		}
+		if(transform.localScale == new Vector3(.01f, .01f, .01f))Destroy(gameObject);
 	}
 }
