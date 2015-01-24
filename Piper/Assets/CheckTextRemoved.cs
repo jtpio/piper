4b1d0ac8f0ef1d4af9d6bbc9de4a3a2ff4192126
Husenap <Hussein_t_97@hotmail.com>
2015-01-24 22:54:47 +0100

    level three setup

---
+++ Piper/Assets/CheckTextRemoved.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckTextRemoved : MonoBehaviour {
+
+	public ParticleSystem ps;
+	
+	void Update () {
+		if(!GameObject.Find("ExplodingBox")){
+			//Trigger events below
+		}
+	}
+	
+	//Trigger one: spawn ps -> disable Cube_Map
+	
+	//Trigger two: move piper in front of player
+	
+	//Trigger three: fade to black
+	
+}
