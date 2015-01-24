6322e5172f4f5ac7ca9bfe8f6ac9c1b6503f7e70
Husenap <Hussein_t_97@hotmail.com>
2015-01-24 20:25:36 +0100

    cursors hopefully fixed

--- Piper/Assets/Player/DetectDevice.cs
+++ Piper/Assets/Player/DetectDevice.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class DetectDevice : MonoBehaviour {
 
+	public GameObject[] cursors = new GameObject[2];
+
 	void Start () {
 		if(GetComponent<Cardboard>()){
 			Cardboard cb = GetComponent<Cardboard>();
-			if(SystemInfo.deviceType == DeviceType.Desktop){
+			if(SystemInfo.deviceType != DeviceType.Desktop){
 				cb.VRModeEnabled = false;
+				foreach(GameObject cursor in cursors)cursor.SetActive(false);
 			}else{
 				cb.VRModeEnabled = true;
 			}
