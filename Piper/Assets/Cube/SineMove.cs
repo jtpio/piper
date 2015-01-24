2b6f83e564b34b1261d16ed4d2e2b719279e2da4
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2015-01-23 21:30:27 -0800

    First commit

---
+++ Piper/Assets/Cube/SineMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SineMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(0, 0, Mathf.Cos(Time.time) * 50);
+	}
+}
