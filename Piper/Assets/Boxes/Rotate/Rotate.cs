f60c95b0b357fccf99e7646adb9d986fa33a9294
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2015-01-24 13:03:19 -0800

    Rotate box after 2 seconds

--- Piper/Assets/Boxes/Rotate/Rotate.cs
+++ Piper/Assets/Boxes/Rotate/Rotate.cs
@@ -4,17 +4,39 @@ using System.Collections;
 [RequireComponent(typeof(Collider))]
 public class Rotate : MonoBehaviour {
 
+	public float rotationDelay;
+
 	private IsLookedAt detector;
+	private bool isRotating = false;
 	
 	void Start() {
+		iTween.Init (gameObject);
 		detector = GetComponent<IsLookedAt>();
 	}
 	void Update() {
 		if (detector.Spotted) {
 			renderer.material.color = Color.yellow;
-			iTween.RotateBy(gameObject, iTween.Hash("x", .25, "easeType", "easeInOutBack"));
+			if (!isRotating) {
+				isRotating = true;
+				StartCoroutine(RotateBox(rotationDelay));
+			}
 		} else {
+			isRotating = false;
+			StopCoroutine("RotateBox");
 			renderer.material.color = Color.white;
 		}
+	}
+
+	IEnumerator RotateBox (float delay) {
+		yield return new WaitForSeconds(delay);
+		if (detector.Spotted) { // double check
+			iTween.RotateBy(gameObject, iTween.Hash("x", .25, "easeType", "easeInOutBack", "oncomplete", "onRotationComplete"));
+		} else {
+			isRotating = false;
+		}
+	}
+
+	void onRotationComplete() {
+		isRotating = false;
 	}
 }
