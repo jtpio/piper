44821d56fa6b1d1db69b4db5b252e5f7e624a489
Husenap <Hussein_t_97@hotmail.com>
2015-01-24 19:46:42 +0100

    Level 3 + camera fix

---
+++ Piper/Assets/DetectDevice.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectDevice : MonoBehaviour {
+
+	void Start () {
+		if(GetComponent<Cardboard>()){
+			Cardboard cb = GetComponent<Cardboard>();
+			if(SystemInfo.deviceType == DeviceType.Desktop){
+				cb.VRModeEnabled = false;
+			}else{
+				cb.VRModeEnabled = true;
+			}
+		}
+	}
+}
