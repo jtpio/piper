a20a160803609812ec9776ad940a3fa46639b170
Husenap <Hussein_t_97@hotmail.com>
2015-01-25 03:21:46 +0100

    stuff

--- Piper/Assets/Player/DetectDevice.cs
+++ Piper/Assets/Player/DetectDevice.cs
@@ -8,7 +8,7 @@ public class DetectDevice : MonoBehaviour {
 	void Start () {
 		if(GetComponent<Cardboard>()){
 			Cardboard cb = GetComponent<Cardboard>();
-			if(SystemInfo.deviceType != DeviceType.Desktop){
+			if(SystemInfo.deviceType == DeviceType.Desktop){
 				cb.VRModeEnabled = false;
 //				foreach(GameObject cursor in cursors)cursor.SetActive(false);
 			}else{
