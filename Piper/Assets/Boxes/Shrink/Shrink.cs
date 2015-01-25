b039b0bd09f8a12bb359d871c3f65f224370ce37
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2015-01-24 17:18:40 -0800

    shrink stuff

--- Piper/Assets/Boxes/Shrink/Shrink.cs
+++ Piper/Assets/Boxes/Shrink/Shrink.cs
@@ -3,17 +3,44 @@ using System.Collections;
 
 public class Shrink : MonoBehaviour {
 
+	public float shrinkSpeed;
+	public float scaleDelay;
+	public int[] sizes = new int[3];
+
 	private IsLookedAt detector;
-	
+	private bool lastSpotted = false;
+	private bool isScaling = false;
+
 	void Start () {
+		iTween.Init (gameObject);
 		detector = GetComponent<IsLookedAt>();
 	}
 	
 	void Update () {
 		if(detector.Spotted){
-			iTween.ScaleBy(gameObject, iTween.Hash("x", .01, "y", .01, "z", .01, "easeType", "easeOutExpo", "Time", 1.3));
-			GetComponent<AudioSource>().Play();
+			if (!isScaling) {
+				isScaling = true;
+				StartCoroutine(ScaleBox(scaleDelay));
+			}
+			renderer.material.color = Color.red;
+		} else {
+			isScaling = true;
+			StopCoroutine("ScaleBox");
+			renderer.material.color = Color.white;
 		}
-		if(transform.localScale == new Vector3(.01f, .01f, .01f))Destroy(gameObject);
+	}
+
+	IEnumerator ScaleBox (float delay) {
+		yield return new WaitForSeconds(delay);
+		if (detector.Spotted) { // double check
+			int scale = Random.Range(0, sizes.Length);
+			iTween.ScaleBy(gameObject, iTween.Hash("x", scale, "y", scale, "z", scale, "easeType", "easeOutExpo", "Time", 1.3));
+		} else {
+			isScaling = false;
+		}
+	}
+
+	void onRotationComplete() {
+		isScaling = false;
 	}
 }
