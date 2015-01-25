3a60a7603d2903b323bcc4dd229574f10bb7ce53
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2015-01-24 19:41:31 -0800

    Little oscillation for the shrinking boxes

---
+++ Piper/Assets/Boxes/Idle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Idle : MonoBehaviour {
+
+	public float amplitude;
+
+	Vector3 initPosition;
+	float phase;
+
+	void Start () {
+		initPosition = transform.position;
+		phase = Random.Range(0.0f, Mathf.PI);
+	}
+	
+	void Update () {
+		transform.position = initPosition + new Vector3(0.0f, amplitude * Mathf.Sin(Time.time + phase), 0.0f);
+	}
+}
