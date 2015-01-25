fbc7a58bdf1e6d1aabaaa1a551ae57027d9a99fd
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2015-01-25 13:55:00 -0800

    Icon and Level1 start by focusing

--- Piper/Assets/GM/Intro/ButtonScript.cs
+++ Piper/Assets/GM/Intro/ButtonScript.cs
@@ -4,15 +4,31 @@ using System.Collections;
 public class ButtonScript : MonoBehaviour {
 
 	CardboardHead head;
-	
+	IsLookedAt isLookedAt;
+
+	bool focused = false;
+
+	void Start () {
+		isLookedAt = GetComponent<IsLookedAt>();
+	}
+
 	void Update () {
 		head = Camera.main.GetComponent<StereoController>().Head;
 	
 		RaycastHit hit;
-		
-		if(collider.Raycast(head.Gaze, out hit, Mathf.Infinity) && Cardboard.SDK.CardboardTriggered){
-			AutoFade.LoadLevel(1, 3, 2, Color.black);
+
+		if (isLookedAt.Spotted) {
+			StartCoroutine(StartGame(3.0f));
+		} else {
+			StopCoroutine("StartGame");
 		}
 		
+	}
+
+	IEnumerator StartGame (float delay) {
+		yield return new WaitForSeconds(delay);
+		if (isLookedAt.Spotted) {
+			AutoFade.LoadLevel(1, 3, 2, Color.black);
+		}
 	}
 }
